043dcebf24eb0351d52a0f5985e674db4caa444c
Nada Saleh <nada.saleh@mazad.at>
2023-08-02 22:54:37 +0300

    add authentication middle ware

---
+++ src/middleWares/auth.ts
@@ -0,0 +1,46 @@
+import config from "config";
+import express from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
+
+import { merge } from "lodash";
+
+import messages from "../utils/messages";
+import User from "../models/user";
+
+const auth = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  // Get the token from the Authorization header
+  const token = req.headers.authorization?.split(" ")[1];
+
+  // If there's no token, return an error
+  if (!token) {
+    return res
+      .status(401)
+      .send({ message: messages.general.missingFieldErr("Access token") });
+  }
+
+  try {
+    // Verify the token using the JWT secret key
+    const decoded = jwt.verify(
+      token,
+      config.get("secrets.jwtPrivateKey")
+    ) as JwtPayload;
+
+    const user = await User.findById(decoded.user.id).lean();
+
+    // Add the decoded user object to the request object for further use
+    merge(req, { user: decoded.user });
+
+    // Call the next middleware function
+    next();
+  } catch (err) {
+    const { name, message } = err;
+    // If the token is invalid, return an error
+    return res.status(401).send({ message: `${name}: ${message}` });
+  }
+};
+
+export default auth;
